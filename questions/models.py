e852f1a2e92d63cee8384c4150d2e64fbd2249a2
Kevin Lee <leekevin@berkeley.edu>
2016-08-20 00:19:51 -0700

    Complete search

--- questions/models.py
+++ questions/models.py
@@ -5,6 +5,11 @@ from django.contrib.postgres.fields import JSONField
 from markdownx.models import MarkdownxField
 
 class Thread(models.Model):
+
+    class Meta:
+        get_latest_by = 'last_edited'
+        ordering = ['last_edited', 'title']
+
     title = models.CharField(max_length=50)
 
     response = MarkdownxField()
@@ -15,3 +20,6 @@ class Thread(models.Model):
     # inside the extra field become stable, then it is time to promote the
     # item(s) to separate fields.
     extra = JSONField(blank=True,null=True)
+
+    def __str__(self):
+        return self.title
--- questions/views.py
+++ questions/views.py
@@ -8,8 +8,6 @@ from django.forms.models import model_to_dict
 
 from .models import Thread
 
-from whoosh.qparser import QueryParser
-
 class HomeView(TemplateView):
 
     template_name = 'questions/home.html'
@@ -21,16 +19,18 @@ class HomeView(TemplateView):
 class SearchHandler(View):
     def get(self, request, query=None):
         """Returns a json of the search results"""
-        ret = {'answers': []}
         assert type(query) == str or query is None, \
             'Query must be a string type or None!'
+
+        ret = {'answers': []}
 
         if query:
             ret['answers'] = [
                 model_to_dict(thread, fields=('title', 'response')) for thread
-                in Thread.objects.annotate(
-                    similarity=TrigramSimilarity('title', query),
-                    ).filter(similarity__gt=0.7).order_by('-similarity')
+                in Thread.objects.annotate(similarity=\
+                    TrigramSimilarity('response', query) +
+                    TrigramSimilarity('title', query),
+                ).filter(similarity__gt=0.1).order_by('-similarity')
             ]
         else:
             ret['answers'] = [
