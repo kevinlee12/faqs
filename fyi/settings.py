16664cc0298ec465ec06485fa39e60e7f48fbcec
Kevin Lee <leekevin@berkeley.edu>
2016-10-16 00:21:00 -0700

    Add circleci settings

--- fyi/settings.py
+++ fyi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -92,6 +93,9 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+if os.environ.get('DATABASE_URL'):
+    DATABASES['default'] = dj_database_url.config()
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
